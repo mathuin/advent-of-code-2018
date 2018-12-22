bb21a3fc5f083f25a89c82fe89390ade6396765a
Jack Twilley <mathuin@gmail.com>
2018-12-21 16:10:33 -0800

    Day 19 part 2!
    I figured out what the program was supposed to do and used alternate 
    means to calculate the final answer.  This code will do it, if you have 
    forever to wait!

--- Day19.js
+++ Day19.js
@@ -7,8 +7,7 @@ const opcodes = require('opcodes');
 
 // Revisiting Day 16's assembly language stuff.
 
-// Puzzle input has an instruction pointer setting in line one and assembly
-// commands in each following line.
+// Test input is unrelated to the puzzle input.
 
 let [ testIPR, testProgram ] = opcodes.parseFile('./Day19-test.txt');
 let [ testIDX, testReg ] = opcodes.execProgram(testProgram, { ipR: testIPR });
@@ -16,8 +15,17 @@ if (testReg[0] !== 6) {
   console.log(`test 1 failed: expected 6, got ${testReg[0]}`);
   process.exit();
 }
+
+// Puzzle input has an instruction pointer setting in line one and assembly
+// commands in each following line.
+
+// News flash: the program computes the sums of the factors of a number.
+// For register 0, that number is 1017, the factors are 1, 3, 9, 113, 339, and 1017, and the sums of the factors is 1482.
+// For register 1, that number is 10551417, the factors are 1, 3, 3517139, and 10551417, and the sums of the factors is 14068560.
+
 let [ realIPR, realProgram ] = opcodes.parseFile('./Day19-input.txt');
 let [ oneIDX, oneReg ] = opcodes.execProgram(realProgram, { ipR: realIPR });
 console.log(`Step one: ${oneReg[0]} was left in register 0`);
-//
-// let [ twoIDX, twoReg ] = opcodes.execProgram(realIPR, realProgram, { reg: [1, 0, 0, 0, 0, 0] });
+
+let [ twoIDX, twoReg ] = opcodes.execProgram(realProgram, { ipR: realIPR, reg: [1, 0, 0, 0, 0, 0] });
+console.log(`Step one: ${twoReg[0]} was left in register 0`);
